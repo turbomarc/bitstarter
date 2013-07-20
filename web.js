87bc40d4b2eaf8049ff7381f4c1b4b5a3444204d
Marc Phillips <marc@thinkrelevance.com>
2013-07-20 01:41:34 +0000

    Populate styles page with content

--- web.js
+++ web.js
@@ -1,8 +1,10 @@
+
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/assets'));
 app.get('/', function(request, response) {
   var content = fs.readFileSync('index.html', 'utf-8');
   response.send(content);
